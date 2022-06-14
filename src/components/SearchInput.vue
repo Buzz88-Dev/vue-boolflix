92907e4bdbbe740baf7193f4f7cc41f752232f1b
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-14 15:38:12 +0200

    created SearchInput

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click.prevent="resetSearch()">Clear</button>
+        <input type="text" placeholder="Search" v-model="inputText" @keyup.enter="$emit('mySearch', inputText)" >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchInput',
+
+    data(){
+        return {
+            inputText : "",           
+        }
+    },
+
+    methods: {
+        resetSearch(){
+            this.inputText = "";
+            this.$emit('mySearch', this.inputText);
+        }
+    }
+}
+</script>
+
+<style  scoped lang="scss">
+
+    
+</style>
