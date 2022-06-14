22b21399e69eefff125431ecdc1866c9acffc898
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-14 15:38:05 +0200

    created SearchInput

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,40 +1,84 @@
 <template>
-  <div class="discs_list">
-            <TheLordOfTheRings v-for="(film, index) in detailsTheLordOfTheRings" :key="index" :itemLordOfTheRings="film" />
-     </div>
+  <div class="container">
+    <div class="search_input">
+      <SearchInput @mySearch="searchMovie"/>
+    </div>
+    <div class="movies_list">
+        <TheLordOfTheRings v-for="(film, index) in filteredDetailsMovies" :key="index" :itemLordOfTheRings="film" />
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import TheLordOfTheRings from './TheLordOfTheRings.vue';
+import SearchInput from './SearchInput.vue';
 
 export default {
   name: 'MoviesFilm',
 
   components: {
     TheLordOfTheRings,
+    SearchInput,
   },
   
   data(){
       return {
           apiUrlTheLordOfTheRings: "https://api.themoviedb.org/3/search/movie?api_key=4f0618c8f1e9a235de03917262238b33&query=il+signore+degli+anelli",
           detailsTheLordOfTheRings: [],
+          userText: "",
       }
   },
 
   created(){
     axios.get(this.apiUrlTheLordOfTheRings)
-    		.then(result => {
-            this.detailsTheLordOfTheRings = result.data.results;
-        		console.log(this.detailsTheLordOfTheRings);    
-    		}).catch(error => {
+    .then(result => {
+        this.detailsTheLordOfTheRings = result.data.results;
+        console.log(this.detailsTheLordOfTheRings);    
+    }).catch(error => {
         console.log("Error", error);
-    	})
+    })
   },
+
+  methods: {
+    searchMovie(textUser){
+       this.userText = textUser;
+       console.log(textUser);
+    }
+  },
+
+  computed: {
+    filteredDetailsMovies (){
+      if (this.userText === ""){
+        return this.detailsTheLordOfTheRings
+      } else {
+        return this.detailsTheLordOfTheRings.filter(item => {
+        return item.original_title.toLowerCase().includes(this.userText.toLowerCase())
+      })
+      }
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+  .container {
+    background-color: rgb(33, 39, 65);
+    padding-top: 40px;
+    padding-bottom: 20px;
+  }
+
+  .search_input {
+    text-align: center;
+  }
+  .movies_list {
+    width: 80%;
+    display: flex;
+    flex-wrap: wrap;
+    margin: auto;
+    color: white;
+  }
 
 </style>
