cc3fe90a8bd2050c289390367e4797b6eec0ac57
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-14 15:38:18 +0200

    created SearchInput

--- src/components/TheLordOfTheRings.vue
+++ src/components/TheLordOfTheRings.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <div class="card">    											
-        <h3>{{ itemLordOfTheRings.original_title }}</h3>
-  	</div>
+  <div class="card"> 
+    <!-- <img :src="itemLordOfTheRings.poster_path" :alt="itemLordOfTheRings.original_title"> -->
+    <div class="text">
+      <h3>Titolo Originale: {{ itemLordOfTheRings.original_title }}</h3>
+      <h3>Titolo: {{ itemLordOfTheRings.title }}</h3>
+      <h3>Voto: {{itemLordOfTheRings.vote_average}}</h3>
+      <h3>Lingua originale: {{itemLordOfTheRings.original_language}}</h3>
+      <!-- <p>{{itemLordOfTheRings.overview}}</p> -->
+    </div> 											
   </div>
 </template>
 
@@ -20,4 +25,22 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
+  .card {
+    width: calc((100% /4) - 40px);
+    margin: 0  20px 0px 20px;
+  }
+  .text {
+    display: flex;
+    flex-direction: column;
+    padding: 20px 0px;
+
+    h3 {
+      font-size: 12px;
+      padding-top: 10px;
+    }
+
+    p {
+      font-size: 10px;
+    }
+  }
 </style>
